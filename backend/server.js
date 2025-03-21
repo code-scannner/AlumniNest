eb7c1de4d0c0ce80afc6e58d98fa1598985e3adf
code-scanner <cyber.gupta001@gmail.com>
2025-03-22 00:05:35 +0530

    developed the cors

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const Authrouter = require("./routes/AuthRoutes");
 const connectDB = require("./db/db");
+
 app.use(express.json());
+app.use(cors({ origin: "*" })); // ✅ Allow all origins
 
 connectDB();
 
 app.use("/api/", Authrouter);
+
 // 🌍 Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
