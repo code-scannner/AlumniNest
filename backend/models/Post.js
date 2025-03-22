504aebf32cd3a234f9d2171fcdae43fbc8903c21
code-scanner <cyber.gupta001@gmail.com>
2025-03-22 15:24:15 +0530

    like and post schema designed

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
     alumni_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Alumni', required: true },
     content: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+    image: { type: String },
+    timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Post', PostSchema);
