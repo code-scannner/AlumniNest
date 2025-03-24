497202224e5baf6f92f29881bfb659a3edaab73b
Utkarsh Trivedi <trivediutkarsh31@gmail.com>
2025-03-24 17:27:37 +0530

    student_profile

--- app/pages/profile.jsx
+++ app/pages/profile.jsx
@@ -46,7 +46,7 @@ export default function ProfilePage() {
         {/* Profile Picture with Edit Icon */}
         <View style={styles.profileWrapper}>
           <Image
-            source={require("../../assets/images/student_logo.png")}
+            source={require("../../assets/images/photo.jpeg")}
             style={styles.profilePic}
           />
         </View>
@@ -58,35 +58,45 @@ export default function ProfilePage() {
         </TouchableOpacity>
 
         {/* Name */}
-        <Text className="text-3xl font-bold italic text-primary-600 mb-4">
-          {res?.full_name}
+        <Text className="text-3xl font-bold italic text-primary-700">
+          Utkarsh Trivedi
         </Text>
 
         {/* Personal Information */}
         <View
           style={styles.infoContainer}
-          className="bg-primary-200 rounded-lg"
+          className="flex-1 justify-center items-center"
         >
-          <Text style={styles.sectionTitle}>Personal Information</Text>
-          <Text style={styles.infoText}>
+          <Text style={styles.bio} className="italic text-primary-200">
+            "{res.bio}"
+          </Text>
+        </View>
+        <View
+          style={styles.infoContainer}
+          className="bg-primary-999 rounded-lg"
+        >
+          <Text style={styles.sectionTitle} className="text-primary-200">
+            Personal Information
+          </Text>
+          <Text style={styles.infoText} className="text-primary-200">
             <Feather name="user" size={16} /> Username: {res.username}
           </Text>
-          <Text style={styles.infoText}>
+          <Text style={styles.infoText} className="text-primary-200">
             <Feather name="mail" size={16} /> Email: {res.email}
           </Text>
-          <Text style={styles.infoText}>
+          <Text style={styles.infoText} className="text-primary-200">
             <Feather name="phone" size={16} /> Phone: {res.phone_no}
           </Text>
-          <Text style={styles.infoText}>
+          <Text style={styles.infoText} className="text-primary-200">
             <Feather name="book" size={16} /> Course: {res.course}
           </Text>
-          <Text style={styles.infoText}>
+          <Text style={styles.infoText} className="text-primary-200">
             <Feather name="layers" size={16} /> Branch: {res.branch}
           </Text>
-          <Text style={styles.infoText}>
+          <Text style={styles.infoText} className="text-primary-200">
             <Feather name="home" size={16} /> College: {res.college}
           </Text>
-          <Text style={styles.infoText}>
+          <Text style={styles.infoText} className="text-primary-200">
             <Feather name="calendar" size={16} /> Passout Year:{" "}
             {res.passout_year}
           </Text>
@@ -94,20 +104,38 @@ export default function ProfilePage() {
 
         {/* Utilities */}
         <View style={styles.utilitiesContainer}>
-          <TouchableOpacity style={styles.utilityButton}>
-            <Feather name="download" size={20} color="#1565C0" />
-            <Text style={styles.utilityText}>Downloads</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.utilityButton}>
-            <Feather name="users" size={20} color="#1565C0" />
-            <Text style={styles.utilityText}>Connections</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.utilityButton}>
-            <Feather name="help-circle" size={20} color="#1565C0" />
-            <Text style={styles.utilityText}>Help Desk</Text>
+          <TouchableOpacity
+            style={styles.utilityButton}
+            className="bg-primary-999"
+          >
+            <Text className="text-primary-200">
+              <Feather name="download" size={20} />
+            </Text>
+            <Text style={styles.utilityText} className="text-primary-200">
+              Downloads
+            </Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.utilityButton}
+            className="bg-primary-999"
+          >
+            <Text className="text-primary-200">
+              <Feather name="users" size={20} />
+            </Text>
+            <Text style={styles.utilityText} className="text-primary-200">Connections</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.utilityButton}
+            className="bg-primary-999"
+          >
+            <Text className="text-primary-200">
+              <Feather name="help-circle" size={20} />
+            </Text>
+            <Text style={styles.utilityText} className="text-primary-200">Help Desk</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.utilityButton}
+            className="bg-primary-999"
             onPress={() => router.push("/login")}
           >
             <Feather name="log-out" size={20} color="red" />
@@ -123,10 +151,13 @@ const styles = StyleSheet.create({
   scrollContainer: {
     paddingBottom: 40,
   },
+  bio: {
+    font: "italic",
+    fontSize: 12,
+  },
   container: {
     flexGrow: 1,
     alignItems: "center",
-    backgroundColor: "#E3F2FD",
     padding: 20,
     marginTop: 40,
   },
@@ -158,12 +189,10 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 18,
     fontWeight: "bold",
-    color: "hsl(210, 80%, 33%)",
     marginBottom: 10,
   },
   infoText: {
     fontSize: 14,
-    color: "#0D47A1",
     marginVertical: 5,
   },
   utilitiesContainer: {
@@ -172,14 +201,12 @@ const styles = StyleSheet.create({
   utilityButton: {
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "#BBDEFB",
     padding: 12,
     borderRadius: 10,
     marginVertical: 5,
   },
   utilityText: {
     fontSize: 16,
-    color: "#0D47A1",
     marginLeft: 10,
   },
 });
