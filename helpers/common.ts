2cc0d70b4a6d49e062456411669905c45993f7e4
Utkarsh Trivedi <trivediutkarsh31@gmail.com>
2025-04-10 16:58:53 +0530

    changes

--- helpers/common.ts
+++ helpers/common.ts
@@ -1,7 +1,6 @@
 import { Dimensions } from "react-native";
 
 const { width, height } = Dimensions.get("window");
-
 export const hp = (percentage: any) => {
   return (height * percentage) / 100;
 };
